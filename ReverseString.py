970eb38872500baf0a5c23747fb570a01b63097e
Shahzad R <shahzad.riasat@gmail.com>
2017-04-01 22:36:07 -0500

    Create ReverseString.py

---
+++ ReverseString.py
@@ -0,0 +1,8 @@
+# Reversing a string in most pythonic way 
+
+mystring = 'A poor happy fellow'
+
+def revString (string):
+    return string[::-1]                # extended slicing [start:stop:step]
+    
+revString(A)
